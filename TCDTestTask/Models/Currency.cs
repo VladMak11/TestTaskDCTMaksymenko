a30a53ee5fba9e9f37402bac1e30954c88f7b2f6
Vladyslav Maksymenko <blakcwolf2017@gmail.com>
2023-06-15 11:38:17 +0300

    Add Modelf folder and add 2 model Currency and Market

---
+++ TCDTestTask/Models/Currency.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCDTestTask.Models
+{
+    public class Currency
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public decimal Price { get; set; }
+        public decimal Volume { get; set; }
+        public decimal PriceChange { get; set; }
+        public List<Market> Markets { get; set; }
+    }
+}
